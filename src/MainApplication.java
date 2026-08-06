f5b1472486ec4885679b2ce961b8978d296f04a7
Khang Nguyen <ngkhang0220@gmail.com>
2026-08-07 00:00:51 +0700

    feat: implement employee management functionality

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,14 +1,13 @@
 import manager.EmployeeManager;
-import model.Company;
+import model.Employee;
 import ui.Menu;
+
+import java.util.Comparator;
 
 public class MainApplication {
 
     public static void main(String[] args) {
-        Menu menu = new Menu("Employee Management System");
-
-        menu.addOption("Enter information of company");
-        menu.addOption("Print information of employees in the company");
+        Menu menu = getMenu();
 
         int choice;
         EmployeeManager hrManager = new EmployeeManager();
@@ -22,20 +21,95 @@ public class MainApplication {
             switch (choice) {
                 case 1:
                     System.out.println("\nEnter information of company");
-                    // TODO: Implement company input
+                    hrManager.enterInfoCompany();
                     break;
                 case 2:
-                    System.out.println("\nPrint information of employees in the company");
-                    System.out.printf("|%-14s|%-25s|%-12s|%-14s|%-13s|%-13s|\n", "ID", "Full Name","Role", "Phone Number", "Working Days", "Daily Wage");
-                    hrManager.getInfoOfAllEmployees();
+                    System.out.println("\nAssign employee to a manager");
+                    hrManager.assignEmployeeToDeptManager();
+                    System.out.println("Assign successfully.");
                     break;
-                case 0:
-                    System.out.println("Quit");
+                case 3:
+                    System.out.println("\nAdd/Delete an employee");
+                    // TODO: Implement add a new employee
+                    break;
+                case 4:
+                    System.out.println("\nPrint information of employees in the company");
+                    System.out.printf("|%-14s|%-25s|%-12s|%-14s|%-13s|%-13s|\n", "ID", "Full Name", "Role", "Phone " +
+                            "Number", "Working Days", "Daily Wage");
+                    hrManager.getInfoOfAllEmployees();
+                    System.out.println();
+                    break;
+                case 5:
+                    System.out.println("\nCalculate total salary of all employees: " + hrManager.getAllSalaryOfCompany());
+                    System.out.println();
+                    break;
+                case 6:
+                    System.out.println("\nFind the top salary staff in the company");
+                    hrManager.findTopSalaryStaff();
+                    System.out.println();
+                    break;
+                case 7:
+                    System.out.println("\nFind the manager has the most number of managed staff");
+                    hrManager.findManagerWithMostStaff();
+                    System.out.println();
+                    break;
+                case 8:
+                    System.out.println("\nSort employees by name with ascending order");
+                    System.out.printf("|%-14s|%-25s|%-12s|%-14s|%-13s|%-13s|\n",
+                            "ID", "Full Name", "Role", "Phone Number", "Working Days", "Daily Wage");
+                    hrManager.sortEmployees(Comparator.comparing(Employee::getFullName));
+                    System.out.println();
+                    break;
+                case 9:
+                    System.out.println("\nSort employees by salary with descending order");
+                    System.out.printf("|%-14s|%-25s|%-12s|%-14s|%-13s|%-13s|\n",
+                            "ID", "Full Name", "Role", "Phone Number", "Working Days", "Daily Wage");
+                    hrManager.sortEmployees(Comparator.comparing(Employee::calculateMonthlySalary).reversed());
+                    System.out.println();
+                    break;
+                case 10:
+                    System.out.println("\nFind the director has the most shared percentage");
+                    hrManager.findDirectorWithMostSharePercent();
+                    System.out.println();
+                    break;
+                case 11:
+                    System.out.println("\nCalculate the income of the director: ");
+                    System.out.printf("|%-14s|%-25s|%-14s|%-13s|%-13s|%-12s|%-12s|\n",
+                            "ID", "Full Name", "Phone Number", "Working Days", "Daily Wage", "Share Percent (%)",
+                            "Income");
+                    hrManager.printIncomeOfDeptDirector();
+                    System.out.println();
+                    break;
+                case 12:
+                    hrManager.printExtraInformationOfEmployees();
+                    System.out.println();
+                    break;
+                case 13:
+                    System.out.println("Bye bye, see you next time!");
                     break;
             }
 
-        } while (choice != 0);
+        } while (choice != menu.getCountOption());
+    }
 
+    private static Menu getMenu() {
+        Menu menu = new Menu("Employee Management System");
+
+        menu.addOption("Enter information of company");
+        menu.addOption("Assign employee to a manager");
+        menu.addOption("Add/Delete an employee");
+        menu.addOption("Print information of employees in the company");
+        menu.addOption("Calculate total salary of all employees");
+        menu.addOption("Find the top salary staff in the company");
+        menu.addOption("Find the manager has the most number of managed staff");
+        menu.addOption("Sort employees by name with ascending order");
+        menu.addOption("Sort employees by salary with descending order");
+        menu.addOption("Find the director has the most shared percentage");
+        menu.addOption("Calculate the income of the director");
+        menu.addOption("Print extra information of employees by role");
+        menu.addOption("Quit");
+
+        return menu;
     }
 
 }
--- src/manager/EmployeeManager.java
+++ src/manager/EmployeeManager.java
@@ -1,19 +1,21 @@
 package manager;
 
-import model.Director;
-import model.Employee;
-import model.Manager;
-import model.Staff;
+import model.*;
+import util.InputHelper;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EmployeeManager {
 
-    private List<Employee> employees;
+    private Company company;
+    private final List<Employee> employees;
 
     public EmployeeManager() {
+        this.company = new Company();
         this.employees = new ArrayList<>(initialData());
     }
 
@@ -38,6 +40,52 @@ public class EmployeeManager {
         return Arrays.asList(director1, director2, manager1, manager2, staff1, staff2, staff3, staff4);
     }
 
+    public void enterInfoCompany() {
+        company.setName(InputHelper.readLine("Enter company name", "Name"));
+        company.setTaxCode(InputHelper.readLine("Enter company tax code", "\\d{10,13}", "Tax Code must be 10 to 13 digits"));
+        company.setMonthlyRevenue(InputHelper.readDouble("Enter monthly revenue", "Monthly Revenue"));
+        company.showInfo();
+    }
+
+    /**
+     * Assign a staff to a manager
+     */
+    public void assignEmployeeToDeptManager() {
+        Manager manager = null;
+        Staff staff = null;
+
+        String managerId = InputHelper.readLine("Enter manager ID", "ID");
+
+        for (Employee employee : employees) {
+            if (employee instanceof Manager && employee.getId().equals(managerId)) {
+                manager = (Manager) employee;
+                break;
+            }
+        }
+
+        if (manager == null) {
+            System.out.println("Invalid manager ID. Return to main menu.");
+            return;
+        }
+
+        String staffId = InputHelper.readLine("Enter staff ID", "ID");
+
+        for (Employee employee : employees) {
+            if (employee instanceof Staff && employee.getId().equals(staffId)) {
+                staff = (Staff) employee;
+                break;
+            }
+        }
+
+        if (staff == null) {
+            System.out.println("Invalid staff ID. Return to main menu.");
+            return;
+        }
+
+        manager.setManagedStaff(staff);
+        staff.setManager(manager);
+    }
+
     /**
      * Print information of all employees
      */
@@ -46,5 +94,163 @@ public class EmployeeManager {
             employee.showInfo();
             System.out.println("|");
         }
+    }
+
+    /**
+     * Get the total salary of all employees in the company
+     */
+    public double getAllSalaryOfCompany() {
+        double totalSalary = 0;
+
+        for (Employee employee : employees) {
+            totalSalary += employee.calculateMonthlySalary();
+        }
+
+        return totalSalary;
+    }
+
+    /**
+     * Find the staffs with the highest salary
+     */
+    public void findTopSalaryStaff() {
+        ArrayList<Staff> topSalaryStaffs = new ArrayList<>();
+        double maxSalary = 0;
+
+        for (Employee employee : employees) {
+            if (employee instanceof Staff && employee.calculateMonthlySalary() >= maxSalary) {
+                maxSalary = employee.calculateMonthlySalary();
+                topSalaryStaffs.add((Staff) employee);
+            }
+        }
+
+        if (topSalaryStaffs.size() != 0) {
+            System.out.println("The staffs with the highest salary are:");
+            System.out.printf("|%-14s|%-25s|%-12s|%-14s|%-13s|%-13s|%-25s|\n",
+                    "ID", "Full Name", "Role", "Phone Number", "Working Days", "Daily Wage", "Manager");
+            for (Staff staff : topSalaryStaffs) {
+                staff.showExtraInfo();
+            }
+
+        } else {
+            System.out.println("No staff has the highest salary.");
+        }
+    }
+
+    /**
+     * Find the managers with the most staff members managed
+     */
+    public void findManagerWithMostStaff() {
+        ArrayList<Manager> managerWithMostStaffs = new ArrayList<>();
+        int maxStaffCount = 0;
+
+        for (Employee employee : employees) {
+            if (employee instanceof Manager) {
+                Manager manager = (Manager) employee;
+                int staffCount = manager.getManagedStaffCount();
+
+                if (staffCount >= maxStaffCount) {
+                    maxStaffCount = staffCount;
+                    managerWithMostStaffs.add(manager);
+                }
+            }
+        }
+
+        if (managerWithMostStaffs.size() != 0) {
+            System.out.println("The managers with the most staff are: ");
+            System.out.printf("|%-14s|%-25s|%-12s|%-14s|%-13s|%-13s|%-12s|\n",
+                    "ID", "Full Name", "Role", "Phone Number", "Working Days", "Daily Wage", "No. of staff");
+            for (Manager manager : managerWithMostStaffs) {
+                manager.showExtraInfo();
+            }
+        } else {
+            System.out.println("No manager has the most staff.");
+        }
+    }
+
+    /**
+     * Sort employees
+     */
+    public void sortEmployees(Comparator<Employee> comparator) {
+        ArrayList<Employee> sortedList = new ArrayList<>(employees);
+
+        sortedList.stream().sorted(comparator).forEach(employee -> {
+            employee.showInfo();
+            System.out.println();
+        });
+    }
+
+    /**
+     * Find the directors with the most share percent (%)
+     */
+    public void findDirectorWithMostSharePercent() {
+        ArrayList<Director> directorWithMostSharePercents = new ArrayList<>();
+        double maxSharePercent = 0;
+
+        for (Employee employee : employees) {
+            if (employee instanceof Director) {
+                Director director = (Director) employee;
+                double sharePercent = director.getSharePercent();
+                if (sharePercent >= maxSharePercent) {
+                    maxSharePercent = sharePercent;
+                    directorWithMostSharePercents.add(director);
+                }
+            }
+        }
+
+        if (directorWithMostSharePercents.size() != 0) {
+            System.out.println("The directors with the most share percent are: ");
+            System.out.printf("|%-14s|%-25s|%-12s|%-14s|%-13s|%-13s|%-12s|\n",
+                    "ID", "Full Name", "Role", "Phone Number", "Working Days", "Daily Wage", "Share Percent (%)");
+            for (Director director : directorWithMostSharePercents) {
+                director.showExtraInfo();
+            }
+        } else {
+            System.out.println("No director has the most share percent.");
+        }
+    }
+
+    /**
+     * Print the income of all directors
+     */
+    public void printIncomeOfDeptDirector() {
+        double companyProfit = company.calculateCompanyProfit(getAllSalaryOfCompany());
+        for (Employee employee : employees) {
+            if (employee instanceof Director) {
+                Director director = (Director) employee;
+                System.out.printf("|%-14s|%-25s|%-14s|%-13.1f|%-13.2f|%-17.2f|%-12.2f|\n",
+                        director.getId(), director.getFullName(), director.getPhoneNumber(),
+                        director.getWorkingDays(), director.getDailyWage(), director.getSharePercent(),
+                        director.calculateIncome(companyProfit));
+            }
+        }
+    }
+
+    /**
+     * Print extra information of employees of a certain role
+     */
+    public void printExtraInformationOfEmployees() {
+        String role = InputHelper.readLine("Enter role [STAFF|MANAGER|DIRECTOR]", "(STAFF|MANAGER|DIRECTOR)",
+                "Role must be one of: STAFF, MANAGER, DIRECTOR");
+
+        if (role.equals("STAFF")) {
+            System.out.printf("|%-14s|%-25s|%-12s|%-14s|%-13s|%-13s|%-25s|\n",
+                    "ID", "Full Name", "Role", "Phone Number", "Working Days", "Daily Wage", "Manager");
+            _getEmployeesByType(Staff.class).forEach(Employee::showExtraInfo);
+        } else if (role.equals("MANAGER")) {
+            System.out.printf("|%-14s|%-25s|%-12s|%-14s|%-13s|%-13s|%-12s|\n",
+                    "ID", "Full Name", "Role", "Phone Number", "Working Days", "Daily Wage", "No. of staff");
+            _getEmployeesByType(Manager.class).forEach(Employee::showExtraInfo);
+        } else {
+            System.out.printf("|%-14s|%-25s|%-12s|%-14s|%-13s|%-13s|%-12s|\n",
+                    "ID", "Full Name", "Role", "Phone Number", "Working Days", "Daily Wage", "Share Percent (%)");
+            _getEmployeesByType(Director.class).forEach(Employee::showExtraInfo);
+        }
+    }
+
+    private <T extends Employee> List<T> _getEmployeesByType(Class<T> type) {
+        return employees.parallelStream()
+                .filter(type::isInstance)
+                .map(type::cast)
+                .collect(Collectors.toList());
     }
 }
